220d257abc03030a95f2f8d34d9d62d139bb8b77
bhagavathi12 <bhagavathi1992@gmail.com>
2025-02-12 22:16:29 +0530

    try

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/HV-Assignment/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
