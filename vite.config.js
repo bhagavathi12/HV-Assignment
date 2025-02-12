416bbeb43dbf42edd6a9e561889cad5a1adce850
bhagavathi12 <bhagavathi1992@gmail.com>
2025-02-12 22:04:45 +0530

    try

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
-  base: "/HV-Assignment"
+  base: "/HV-Assignment/"
 })
